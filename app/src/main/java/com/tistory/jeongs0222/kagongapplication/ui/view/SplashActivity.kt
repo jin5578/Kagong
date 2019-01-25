a65a6fafe67a42c965442d01a56fdae12c3d2b1d
정현성 <jin55789@naver.com>
2019-01-25 17:35:43 +0900

    SplashActivity  - 3초 뒤 넘어가는 로딩기능 추가

---
+++ app/src/main/java/com/tistory/jeongs0222/kagongapplication/ui/view/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.tistory.jeongs0222.kagongapplication.ui.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import com.tistory.jeongs0222.kagongapplication.MainActivity
+import com.tistory.jeongs0222.kagongapplication.R
+import com.tistory.jeongs0222.kagongapplication.databinding.ActivitySplashBinding
+import com.tistory.jeongs0222.kagongapplication.ui.viewmodel.SplashViewModel
+
+class SplashActivity : BaseActivity<ActivitySplashBinding>() {
+
+    override val layoutResourceId: Int = R.layout.activity_splash
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val splashViewModel = SplashViewModel()
+
+        splashViewModel.openDelay.observe(this, Observer {
+            if (it)
+                startActivity(Intent(this, MainActivity::class.java))
+        })
+
+        viewDataBinding.viewmodel = splashViewModel
+        viewDataBinding.setLifecycleOwner(this)
+    }
+
+}
---
+++ app/src/main/java/com/tistory/jeongs0222/kagongapplication/ui/viewmodel/SplashViewModel.kt
@@ -0,0 +1,30 @@
+package com.tistory.jeongs0222.kagongapplication.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import com.tistory.jeongs0222.kagongapplication.utils.SingleLiveEvent
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+class SplashViewModel : DisposableViewModel() {
+
+    private val _openDelay = SingleLiveEvent<Boolean>()
+    val openDelay: LiveData<Boolean> get() = _openDelay
+
+    init {
+        addDisposable(delayTime())
+    }
+
+    private fun delayTime(): Disposable =
+        Completable.timer(3000, TimeUnit.MILLISECONDS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                _openDelay.value = true
+
+                //onCleared()
+            }
+
+}
