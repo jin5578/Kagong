60a346fedcf5bc622ee22f48058b56737fa3ff29
정현성 <jin55789@naver.com>
2019-03-03 16:21:32 +0900

    AddDetailScheduleActivity  - Init Design

--- app/src/main/java/com/tistory/jeongs0222/kagongapplication/ui/view/activity/AddDetailScheduleActivity.kt
+++ app/src/main/java/com/tistory/jeongs0222/kagongapplication/ui/view/activity/AddDetailScheduleActivity.kt
@@ -1,6 +1,8 @@
 package com.tistory.jeongs0222.kagongapplication.ui.view.activity
 
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
 import com.tistory.jeongs0222.kagongapplication.R
 import com.tistory.jeongs0222.kagongapplication.databinding.ActivityAddDetailScheduleBinding
 import com.tistory.jeongs0222.kagongapplication.ui.viewmodel.AddDetailScheduleViewModel
@@ -17,9 +19,22 @@ class AddDetailScheduleActivity : BaseActivity<ActivityAddDetailScheduleBinding>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_detail_schedule)
 
-        viewDataBinding.dViewModel = addDetailScheduleViewModel
+        addDetailScheduleViewModel.previousClick.observe(this@AddDetailScheduleActivity, Observer {
+            finish()
+        })
+
+        addDetailScheduleViewModel.moreVisibility.observe(this@AddDetailScheduleActivity, Observer {
+            if(it) {
+                viewDataBinding.moreLayout.visibility = View.VISIBLE
+            } else {
+                viewDataBinding.moreLayout.visibility = View.GONE
+            }
+        })
+
+
+
+        viewDataBinding.adViewModel = addDetailScheduleViewModel
         viewDataBinding.lifecycleOwner = this@AddDetailScheduleActivity
     }
 
--- app/src/main/java/com/tistory/jeongs0222/kagongapplication/ui/viewmodel/AddDetailScheduleViewModel.kt
+++ app/src/main/java/com/tistory/jeongs0222/kagongapplication/ui/viewmodel/AddDetailScheduleViewModel.kt
@@ -1,6 +1,55 @@
 package com.tistory.jeongs0222.kagongapplication.ui.viewmodel
 
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.transition.Visibility
+import com.tistory.jeongs0222.kagongapplication.utils.SingleLiveEvent
+
 
 class AddDetailScheduleViewModel: DisposableViewModel() {
+
+    private val _previousClick = SingleLiveEvent<Any>()
+    val previousClick: LiveData<Any>
+        get() = _previousClick
+
+    private val _moreClick = SingleLiveEvent<Any>()
+    val moreClick: LiveData<Any>
+        get() = _moreClick
+
+    private val _cancelClick = SingleLiveEvent<Any>()
+    val cancelClick: LiveData<Any>
+        get() = _cancelClick
+
+    private val _moreVisibility = MutableLiveData<Boolean>()
+    val moreVisibility: LiveData<Boolean>
+        get() = _moreVisibility
+
+    private val TAG = "AddDetaiViewModel"
+
+
+    fun previousClickEvent() {
+        _previousClick.call()
+
+        Log.e(TAG, "previousClick")
+    }
+
+    fun moreClickEvent() {
+        _moreClick.call()
+
+        _moreVisibility.value = true
+
+        Log.e(TAG, "moreClick")
+    }
+
+    fun cancelClickEvent() {
+        _cancelClick.call()
+
+        _moreVisibility.value = false
+
+        Log.e(TAG, "cancelClick")
+    }
+
 
 }
